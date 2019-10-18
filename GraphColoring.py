5a08a496334a30d8a19e48986ca27920a6b0c322
AleksandraSwierkowska <48619544+AleksandraSwierkowska@users.noreply.github.com>
2019-10-18 22:35:35 +0200

    Add files via upload

--- GraphColoring.py
+++ GraphColoring.py
@@ -12,6 +12,20 @@ class Graph:
         self.matrix[vertex1-1][vertex2-1] = 1
         self.matrix[vertex2-1][vertex1-1] = 1
 
+def greedy_coloring(graph):
+    for vertex in range(len(graph.matrix)):
+        colors = []
+        for other_vertex in range(len(graph.matrix[vertex])):
+            if graph.matrix[vertex][other_vertex] == 1 and not graph.matrix[other_vertex][other_vertex]in colors:
+                colors.append(graph.matrix[other_vertex][other_vertex])
+        for i in range(1, len(colors)+2):
+            if not i in colors:
+                color = i
+                break
+        graph.color(vertex, color)
+    return graph
+
+
 
 # CREATING GRAPH FROM FILE
 f = open("graf.txt", "r")   # opening the file
@@ -21,5 +35,6 @@ graph = Graph(vertex_number)
 for i in lines[1:]:
     vertices = i.split()
     graph.add_edge(int(vertices[0]), int(vertices[1]))
+graph = greedy_coloring((graph))
 print(graph)
 f.close()
