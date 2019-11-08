5403aabde781359c61a862582ca6ca2e9b275943
Zuza <zuz.trafas@gmail.com>
2019-11-08 21:55:25 +0100

    graph generator and optimized greedy coloring

--- GraphColoring.py
+++ GraphColoring.py
@@ -1,3 +1,6 @@
+import random
+
+
 class Graph:
     def __init__(self, size):
         self.matrix = [[0 for _ in range(size)] for _ in range(size)]
@@ -24,17 +27,71 @@ class Graph:
                     color = i
                     break
             self.color(vertex, color)
+        maximum = 0
+        for c in range(len(self.matrix)):
+            if self.matrix[c][c] > maximum:
+                maximum = self.matrix[c][c]
+        return maximum
 
+    def optimized_greedy_coloring(self):
+        neighbor_list = []
+        vertex_list = list(range(len(self.matrix)))
+        for vertex in range(len(self.matrix)):
+            neighbors = sum(self.matrix[vertex]) - self.matrix[vertex][vertex]
+            neighbor_list.append(neighbors)
+        sorted_vertex_list = [x for _, x in sorted(zip(neighbor_list, vertex_list), reverse=True)]
+        for vertex in sorted_vertex_list:
+            colors = []
+            color = 0
+            for other_vertex in range(len(self.matrix[vertex])):
+                if self.matrix[vertex][other_vertex] == 1 and self.matrix[other_vertex][other_vertex] not in colors:
+                    colors.append(self.matrix[other_vertex][other_vertex])
+            for i in range(1, len(colors) + 2):
+                if i not in colors:
+                    color = i
+                    break
+            self.color(vertex, color)
+        maximum = 0
+        for c in range(len(self.matrix)):
+            if self.matrix[c][c] > maximum:
+                maximum = self.matrix[c][c]
+        return maximum
+
+
+def graph_generator(density, vertex_number, file_name):
+    edge_number = vertex_number
+    f = open(file_name, "w+")
+    f.write(str(vertex_number) + "\n")
+    for i in range(1, vertex_number):
+        f.write(str(i) + " " + str(i+1) + "\n")
+    f.write("1 " + str(vertex_number) + "\n")
+    graph_density = (2 * edge_number) / (vertex_number * (vertex_number - 1))
+    while graph_density <= density:
+        a = random.randint(1, vertex_number)
+        b = random.randint(1, vertex_number)
+        while a == b:
+            b = random.randint(1, vertex_number)
+        f.write(str(a) + " " + str(b) + "\n")
+        edge_number += 1
+        graph_density = (2 * edge_number) / (vertex_number * (vertex_number - 1))
+    f.close()
 
 
 # CREATING GRAPH FROM FILE
-f = open("graf.txt", "r")   # opening the file
+graph_generator(0.5, 500, "a.txt")
+f = open("a.txt", "r")   # opening the file
 lines = f.readlines()   # storing content in lines variable
 vertex_number = int(lines[0])  # first line is vertex number
 graph = Graph(vertex_number)
 for i in lines[1:]:
     vertices = i.split()
     graph.add_edge(int(vertices[0]), int(vertices[1]))
-graph.greedy_coloring()
-print(graph)
+graph2 = Graph(vertex_number)
+for i in lines[1:]:
+    vertices = i.split()
+    graph2.add_edge(int(vertices[0]), int(vertices[1]))
+print(graph.greedy_coloring())
+#print(graph)
+print(graph2.optimized_greedy_coloring())
+#print(graph2)
 f.close()
