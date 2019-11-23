0eed99d75235251f8a616a381df6b3751af8f7cf
Zuza <zuz.trafas@gmail.com>
2019-11-23 22:51:17 +0100

    a lot

--- GraphColoring.py
+++ GraphColoring.py
@@ -10,9 +10,39 @@ class Graph:
     def __str__(self):
         return '\n'.join([''.join(['{:4}'.format(item) for item in row]) for row in self.matrix])
 
+    def __eq__(self, other):
+        return self.matrix == other.matrix
+
+    def __hash__(self):
+        return id(self)
+
     def color(self, vertex, color):
         self.matrix[vertex][vertex] = color
         self.colors[vertex] = color
+
+    def find_errors(self):
+        error_number = 0
+        for vertex in range(len(self.matrix)):
+            for other_vertex in range(vertex+1, len(self.matrix)):
+                if self.matrix[vertex][other_vertex] == 1 and self.colors[vertex] == self.colors[other_vertex]:
+                    error_number += 1
+        return error_number
+
+    def mutation(self):
+        for vertex in range(len(self.matrix)):
+            adjacent_colors = []
+            for v in range(len(self.matrix)):
+                if self.matrix[vertex][v] == 1:
+                    adjacent_colors.append(self.colors[v])
+            for i in range(1, self.colors[vertex]):
+                if i not in adjacent_colors:
+                    self.color(vertex, i)
+            for other_vertex in range(vertex+1, len(self.matrix)):
+                if self.matrix[vertex][other_vertex] == 1 and self.colors[vertex] == self.colors[other_vertex]:
+                    for i in range(1, max(adjacent_colors)+2):
+                        if i not in adjacent_colors:
+                            self.color(vertex, i)
+                            break
 
     def add_edge(self, vertex1, vertex2):
         self.matrix[vertex1-1][vertex2-1] = 1
@@ -30,11 +60,7 @@ class Graph:
                     color = i
                     break
             self.color(vertex % len(self.matrix), color)
-        maximum = 0
-        for c in range(len(self.matrix)):
-            if self.matrix[c][c] > maximum:
-                maximum = self.matrix[c][c]
-        return maximum
+        return max(self.colors)
 
     def optimized_greedy_coloring(self):
         neighbor_list = []
@@ -84,6 +110,45 @@ def graph_generator(density, vertex_number, file_name):
     f.close()
 
 
+def crossover(parent1, parent2):
+    a = random.randint(1, len(parent1.matrix))
+    child = copy.deepcopy(parent1)
+    child.colors[a:] = parent2.colors[a:]
+    return child
+
+
+def parent_selection1(graphs):
+    parents = []
+    for _ in range(2):
+        a = random.randint(1, len(graphs)-1)
+        b = random.randint(1, len(graphs)-1)
+        while a == b:
+            b = random.randint(1, len(graphs)-1)
+        max_a = max(graphs[a].colors)
+        max_b = max(graphs[b].colors)
+        if max_a > max_b:
+            parent = graphs[b]
+        else:
+            parent = graphs[a]
+        parents.append(parent)
+    return parents
+
+
+def parent_selection2(graphs):
+    parents = []
+    max_colors = {}
+    for i in range(len(graphs)):
+        max_colors[graphs[i]] = max(graphs[i].colors)
+    v = list(max_colors.values())
+    k = list(max_colors.keys())
+    parents.append(k[v.index(min(v))])
+    del max_colors[k[v.index(min(v))]]
+    v = list(max_colors.values())
+    k = list(max_colors.keys())
+    parents.append(k[v.index(min(v))])
+    return parents
+
+
 def generate_population(graph_number, file):
     graphs = []
     f = open(file, "r")  # opening the file
@@ -99,26 +164,37 @@ def generate_population(graph_number, file):
     for i in range(len(graphs)):
         graphs[i].greedy_coloring(i)
 
-    return graphs
     f.close()
+    return graphs
 
 
-"""
-# CREATING GRAPH FROM FILE
-#graph_generator(0.7, 10, "a.txt")
-f = open("graph70.txt", "r")   # opening the file
-lines = f.readlines()   # storing content in lines variable
-vertex_number = int(lines[0])  # first line is vertex number
-graph = Graph(vertex_number)
-for i in lines[1:]:
-    vertices = i.split()
-    graph.add_edge(int(vertices[0]), int(vertices[1]))
-graph2 = copy.deepcopy(graph)
-print(graph.greedy_coloring(0))
-#print(graph)
-#print(graph2.greedy_coloring(2))
-#print(graph2)
-f.close()
-"""
-graph_generator(0.7, 10, "a.txt")
-generate_population(5, "a.txt")
+def genetic(graphs):
+    a = random.randint(1, 2)
+    if a == 1:
+        parents = parent_selection1(graphs)
+    else:
+        parents = parent_selection2(graphs)
+    child = crossover(parents[0], parents[1])
+    child.mutation()
+    if child != graphs[-1]:
+        graphs.append(child)
+        maximum = 0
+        max_index = 0
+        for i in range(len(graphs)):
+            if max(graphs[i].colors) > maximum:
+                maximum = max(graphs[i].colors)
+                max_index = i
+        graphs.pop(max_index)
+    return graphs
+
+
+graph_generator(0.7, 80, "a.txt")
+graphs = generate_population(20, "queen6")
+print(max(graphs[0].colors))
+for i in range(1000):
+    graphs = genetic(graphs)
+max_colors = {}
+for i in range(len(graphs)):
+    max_colors[graphs[i]] = max(graphs[i].colors)
+v = list(max_colors.values())
+print(min(v))
